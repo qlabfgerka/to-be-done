484a287604f78413f74aff1a713ffe7de1968c7e
Jan Premzl <jan.premzl16@gmail.com>
2021-11-17 21:05:36 +0100

    Login and registration + routing
    - added login and registration lazy loaded modules
    - changed angular routing

--- to-be-done-frontend/src/app/app-routing.module.ts
+++ to-be-done-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: `login`,
+    loadChildren: () =>
+      import('./components/account/login/login.module').then(
+        (m) => m.LoginModule
+      ),
+  },
+  {
+    path: `register`,
+    loadChildren: () =>
+      import('./components/account/register/register.module').then(
+        (m) => m.RegisterModule
+      ),
+  },
+  {
+    path: ``,
+    loadChildren: () =>
+      import('./components/home-page/home-page.module').then(
+        (m) => m.HomePageModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
